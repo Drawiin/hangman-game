f4f4649b05e9212525b06aa71725c88eaf71de94
viniciussantosguimaraes <vinicius.guimaraes@neon.com.br>
2023-07-13 10:57:36 -0300

    Removing unused deps

--- androidApp/src/test/kotlin/br/com/drawiin/hangmangame/android/game/GameViewModelTest.kt
+++ androidApp/src/test/kotlin/br/com/drawiin/hangmangame/android/game/GameViewModelTest.kt
@@ -1,10 +1,5 @@
 package br.com.drawiin.hangmangame.android.game
 
-import org.junit.jupiter.api.Assertions.*
-
 internal class GameViewModelTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    fun setUp() {
-    }
 }
